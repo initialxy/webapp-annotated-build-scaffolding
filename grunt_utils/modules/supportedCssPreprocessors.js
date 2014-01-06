42655a991b1d09a114c8f60d3b0291e53932822a
Xingchen Yu <initialxy@gmail.com>
2014-01-05 20:22:30 -0500

    Added license.

--- grunt_utils/modules/supportedCssPreprocessors.js
+++ grunt_utils/modules/supportedCssPreprocessors.js
@@ -1,3 +1,11 @@
+/*!
+ * grunt-webapp-less-scaffolding
+ * https://github.com/initialxy/grunt-webapp-less-scaffolding
+ *
+ * Copyright (c) 2013 Xingchen Yu (initialxy -at- gmail.com)
+ * Licensed under the MIT license.
+ */
+
 module.exports = [
     {task: "less", ext: "less"},
     {task: "sass", ext: "sass"}
--- grunt_utils/tasks/uglifyFilesToTarget.js
+++ grunt_utils/tasks/uglifyFilesToTarget.js
@@ -1,3 +1,11 @@
+/*!
+ * grunt-webapp-less-scaffolding
+ * https://github.com/initialxy/grunt-webapp-less-scaffolding
+ *
+ * Copyright (c) 2013 Xingchen Yu (initialxy -at- gmail.com)
+ * Licensed under the MIT license.
+ */
+
 "use strict";
 
 /**
